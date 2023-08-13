1d6a970865c0e36ddc8f210618eb7a2fff9d9974
Muhammad Moshin <moshin.backend@gmail.com>
2023-08-13 03:35:03 -0700

    film entity created.

---
+++ src/db/entities/film.entity.ts
@@ -0,0 +1,50 @@
+import {Entity,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn} from "typeorm";
+
+// enum Genres {
+//     Horror,
+//     Thriller,
+//     Action,
+//     Drama,
+//     Comedy,
+//     Science_Fiction,
+// }
+
+
+@Entity('films')
+export class Film{
+    
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+    
+    @Column()
+    description: string
+    
+    @Column({type:'date'})
+    release_date:string
+    
+    @Column({type:"decimal",precision:1,scale:1})
+    rating:number 
+    
+    @Column({type:"decimal",precision:5,scale:2})
+    ticket_price:number 
+    
+    @Column()
+    country: string
+  
+    @Column()
+    photo: string
+
+    @Column({name:'genre',type:'enum',enum: ['Action','Horror','Thriller','Drama','Comedy','Science Fiction'] })
+    genre:string
+  
+
+    @CreateDateColumn()
+    createdAt: Date;
+ 
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+}
