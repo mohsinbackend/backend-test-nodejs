81565df36f7d54ae7a30daab3dbe63038b227b94
Muhammad Moshin <moshin.backend@gmail.com>
2023-08-13 12:34:31 -0700

    User resource created.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { FilmsModule } from './films/films.module';
 import { DatabaseModule } from './db/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TypeOrmModule,DatabaseModule,FilmsModule],
+  imports: [TypeOrmModule,DatabaseModule,FilmsModule, UsersModule],
 })
 export class AppModule {}
