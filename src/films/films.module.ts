4e9e4b1cd1a40d307579ee38a8322099de73e042
Muhammad Moshin <moshin.backend@gmail.com>
2023-08-13 04:27:53 -0700

    global prefix '/api/' added.

--- src/films/films.module.ts
+++ src/films/films.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FilmsService } from './films.service';
 import { FilmsController } from './films.controller';
+import { DatabaseModule } from 'src/db/database.module';
+
 
 @Module({
+  imports:[DatabaseModule],
   controllers: [FilmsController],
   providers: [FilmsService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  await app.listen(3000)
+  .then(()=>{ console.log(`Server successfully started on 3000 port.`) })
+  .catch(err=>{ console.log(`Server faild error===============> `,err);  })
+
 }
 bootstrap();
